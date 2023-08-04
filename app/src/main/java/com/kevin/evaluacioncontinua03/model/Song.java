c47384b269467a2165016e399997127461a9f9e6
kevindevperu <tiradoatalaya@gmail.com>
2023-08-04 10:40:49 -0500

    Actualizado

--- app/src/main/java/com/kevin/evaluacioncontinua03/model/Song.java
+++ app/src/main/java/com/kevin/evaluacioncontinua03/model/Song.java
@@ -1,18 +1,18 @@
 package com.kevin.evaluacioncontinua03.model;
 
 public class Song extends Music {
-    private int duration;
+    private String duration;
 
-    public Song(String name, String artist, String imgUrl, int duration){
+    public Song(String name, String artist, String imgUrl, String duration){
         super(name, artist, imgUrl);
         this.duration =  duration;
     }
 
-    public int getDuration() {
+    public String getDuration() {
         return duration;
     }
 
-    public void setDuration(int duration) {
+    public void setDuration(String duration) {
         this.duration = duration;
     }
 }
